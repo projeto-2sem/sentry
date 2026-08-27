90708e81e1fe984db35a9f2bc43946b557e5528d
YaghoCodes <yagho.chinaglia@sptech.school>
2026-08-26 23:32:45 -0300

    fix: Fixing Database with new script and new modeling / LDM

--- web-data-viz/src/database/script-tabelas_atualizado.sql
+++ web-data-viz/src/database/script-tabelas_atualizado.sql
@@ -1,13 +1,14 @@
 CREATE DATABASE if not exists sentry;
 USE sentry;
 
-drop table if exists endereco;
-drop table if exists empresa;
 drop table if exists usuario;
 drop table if exists codigo_ativacao;
-drop table if exists tipo_medicao;
 drop table if exists servidor_medicao;
 drop table if exists servidor;
+drop table if exists empresa;
+drop table if exists tipo_medicao;
+drop table if exists sistema_operacional;
+drop table if exists endereco;
 
 CREATE TABLE endereco (
   idEndereco INT PRIMARY KEY AUTO_INCREMENT,
@@ -18,71 +19,81 @@ CREATE TABLE endereco (
   bairro VARCHAR(150) NOT NULL,
   cidade VARCHAR(150) NOT NULL,
   estado CHAR(2) NOT NULL,
-  data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-  data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  data_criacao datetime default current_timestamp,
+  data_atualizacao datetime default current_timestamp on update current_timestamp
 );
-
+ 
+CREATE TABLE sistema_operacional (
+  idSistemaOperacional INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL
+);
+ 
+CREATE TABLE tipo_medicao (
+  idTipoMedicao INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  unidade_medida VARCHAR(45),
+  apelido VARCHAR(45) NOT NULL,
+  descricao TEXT
+);
+ 
 CREATE TABLE empresa (
   idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
   nome_fantasia VARCHAR(255),
   razao_social VARCHAR(255),
   cnpj VARCHAR(18) NOT NULL UNIQUE,
   enderecoId INT NOT NULL,
-  data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-  data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  data_criacao datetime default current_timestamp,
+  data_atualizacao datetime default current_timestamp on update current_timestamp,
   FOREIGN KEY (enderecoId) REFERENCES endereco(idEndereco)
 );
-
+ 
 CREATE TABLE codigo_ativacao (
-  idCodigo INT PRIMARY KEY,
-  codigo_ativacao VARCHAR(10),
-  data_criacao DATETIME,
-  usado TINYINT,
-  empresa_idEmpresa INT NOT NULL,
-  FOREIGN KEY (empresa_idEmpresa) REFERENCES empresa(idEmpresa)
+  idCodigo INT PRIMARY KEY AUTO_INCREMENT,
+  codigo_ativacao VARCHAR(10) NOT NULL UNIQUE,
+  usado TINYINT NOT NULL,
+  empresaId INT NOT NULL,
+  data_criacao datetime default current_timestamp,
+  data_atualizacao datetime default current_timestamp on update current_timestamp,
+  FOREIGN KEY (empresaId) REFERENCES empresa(idEmpresa)
 );
-
+ 
 CREATE TABLE servidor (
   idServidor INT PRIMARY KEY AUTO_INCREMENT,
+  sistemaOperacionalId INT NOT NULL,
+  empresaId INT NOT NULL,
   apelido VARCHAR(255),
   hostname VARCHAR(255),
   mac VARCHAR(17),
-  empresaId INT,
-  modeloId INT,
-  data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-  data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  modelo VARCHAR(45),
+  data_criacao datetime default current_timestamp,
+  data_atualizacao datetime default current_timestamp on update current_timestamp,
+  status TINYINT,
+  FOREIGN KEY (sistemaOperacionalId) REFERENCES sistema_operacional(idSistemaOperacional),
   FOREIGN KEY (empresaId) REFERENCES empresa(idEmpresa)
 );
-
-CREATE TABLE tipo_medicao (
-  idTipoMedicao INT PRIMARY KEY,
-  nome VARCHAR(45),
-  unidade_medida VARCHAR(45),
-  comando_apelido VARCHAR(45),
-  descricao TEXT
-);
-
+ 
 CREATE TABLE servidor_medicao (
   servidorId INT,
-  medicaoId INT,
-  intervalo INT,
+  tipoMedicaoId INT,
   ativo TINYINT,
-  alerta INT,
-  PRIMARY KEY (servidorId, medicaoId),
+  parametro INT,
+  PRIMARY KEY (servidorId, tipoMedicaoId),
   FOREIGN KEY (servidorId) REFERENCES servidor(idServidor),
-  FOREIGN KEY (medicaoId) REFERENCES tipo_medicao(idTipoMedicao)
+  FOREIGN KEY (tipoMedicaoId) REFERENCES tipo_medicao(idTipoMedicao)
 );
-
+ 
 CREATE TABLE usuario (
   idUsuario INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(255),
   email VARCHAR(255),
   senha VARCHAR(255),
-  empresaId INT,
-  papel_usuario VARCHAR(30) NOT NULL DEFAULT 'Analista',
+  empresaId INT NOT NULL,
+  cargo VARCHAR(30) DEFAULT 'Analista',
   responsavel INT,
-  data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-  data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  data_criacao datetime default current_timestamp,
+  data_atualizacao datetime default current_timestamp on update current_timestamp,
+  FOREIGN KEY (empresaId) REFERENCES empresa(idEmpresa),
   FOREIGN KEY (responsavel) REFERENCES usuario(idUsuario),
-  FOREIGN KEY (empresaId) REFERENCES empresa(idEmpresa)
+  CONSTRAINT fk_check_papel CHECK (cargo IN ('Administrador', 'Analista'))
 );
+ 
